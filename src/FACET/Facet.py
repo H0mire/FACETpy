d5c9ded4b65acd9e7f4e900cbeb2e6b802983dd6
H0mire <janikmueller12@gmail.com>
2023-04-15 20:11:24 +0200

    initial Main Commit. Added example and Files with experimental Code

---
+++ src/FACET/Facet.py
@@ -0,0 +1,8 @@
+class Facet:
+	def __init__(self, message):
+		self._message=message
+	def printEEG(self):
+		print("EEG")
+
+	def printMessage(self):
+		print(self._message)
---
+++ src/example1.py
@@ -0,0 +1,10 @@
+from FACET.Facet import Facet
+
+
+def main():
+	f = Facet("Hello")
+	f.printEEG()
+	f.printMessage()
+
+if __name__ == "__main__":
+	main()
